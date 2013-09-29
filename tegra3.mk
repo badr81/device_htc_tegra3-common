9e7a8e37759143417d37ab764910919fe1db5b55
LorDClockaN <davor@losinj.com>
2013-09-29 22:11:25 +0200

    Revert "Add hardware tunable support"
    This reverts commit 1ce38799f3c48a95aa26f72727043ec9f06109bd.
    
    Conflicts:
    	tegra3.mk
    
    Change-Id: I8c9012491b97045170865f5a7a48806f838038b6

--- tegra3.mk
+++ tegra3.mk
@@ -85,10 +85,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
     $(LOCAL_PATH)/prebuilt/usr/keylayout/tegra-kbc.kl:system/usr/keylayout/tegra-kbc.kl
 
-# Common ramdisk files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/init.tegra3-common.rc:root/init.tegra3-common.rc
-
 # Lights
 PRODUCT_PACKAGES += \
     lights.tegra
@@ -134,7 +130,7 @@ PRODUCT_PACKAGES += \
 # Video
 PRODUCT_PACKAGES += \
     libstagefrighthw
-
+    
 # iw
 PRODUCT_PACKAGES += \
     iw 
